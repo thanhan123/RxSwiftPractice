7a3434e20693acb09f47d14b14a2ffda9fe645a1
thanhan123 <thanhan.uit@gmail.com>
2017-04-21 10:35:06 +0700

    finish_RxSwift_networking_example

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -6,54 +6,106 @@
 //  Copyright © 2017 Dinh Thanh An. All rights reserved.
 //
 
+import Moya
+import Moya_ModelMapper
 import UIKit
 import RxCocoa
 import RxSwift
+import Mapper
+
+struct Repository: Mappable {
+    
+    let identifier: Int
+    let language: String
+    let name: String
+    let fullName: String
+    
+    init(map: Mapper) throws {
+        try identifier = map.from("id")
+        try language = map.from("language")
+        try name = map.from("name")
+        try fullName = map.from("full_name")
+    }
+}
+
+struct Issue: Mappable {
+    
+    let identifier: Int
+    let number: Int
+    let title: String
+    let body: String
+    
+    init(map: Mapper) throws {
+        try identifier = map.from("id")
+        try number = map.from("number")
+        try title = map.from("title")
+        try body = map.from("body")
+    }
+}
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    var shownCities = [String]() // Data source for UITableView
-    let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // Our mocked API data source
-    let disposeBag = DisposeBag() // Bag of disposables to release them when view is being deallocated
+    let disposeBag = DisposeBag()
+    var provider: RxMoyaProvider<GitHub>!
+    var latestRepositoryName: Observable<String> {
+        return searchBar
+            .rx.text
+            .orEmpty
+            .debounce(0.5, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+    }
+    
+    var issueTrackerModel: IssueTrackerModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup()
+        setupRx()
     }
     
-    func setup() {
-        tableView.dataSource = self
-        searchBar
-            .rx.text // Observable property thanks to RxCocoa
-            .orEmpty // Make it non-optional
-            .debounce(0.5, scheduler: MainScheduler.instance) // Wait 0.5 for changes.
-            .distinctUntilChanged() // If they didn't occur, check if the new value is the same as old.
-            .filter { !$0.isEmpty } // If the new value is really new, filter for non-empty query.
-            .subscribe(onNext: { [unowned self] query in // Here we subscribe to every new value, that is not empty (thanks to filter above).
-                self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // We now do our "API Request" to find cities.
-                self.tableView.reloadData() // And reload table view data.
-            })
-            .addDisposableTo(disposeBag) // Don't forget to add this to disposeBag. We want to dispose it on deinit.
-    }
-    
-}
-
-// MARK: - UITableViewDataSource
-/// Here we have standard data source extension for ViewController
-extension ViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shownCities.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
-        cell.textLabel?.text = shownCities[indexPath.row]
+    func setupRx() {
+        // First part of the puzzle, create our Provider
+        provider = RxMoyaProvider<GitHub>()
         
-        return cell
+        // Here we tell table view that if user clicks on a cell,
+        // and the keyboard is still visible, hide it
+        tableView
+            .rx.itemSelected
+            .subscribe(onNext: { indexPath in
+                if self.searchBar.isFirstResponder == true {
+                    self.view.endEditing(true)
+                }
+            })
+            .addDisposableTo(disposeBag)
+        
+        // Now we will setup our model
+        issueTrackerModel = IssueTrackerModel(provider: provider, repositoryName: latestRepositoryName)
+        
+        // And bind issues to table view
+        // Here is where the magic happens, with only one binding
+        // we have filled up about 3 table view data source methods
+        issueTrackerModel
+            .trackIssues()
+            .bindTo(tableView.rx.items) { tableView, row, item in
+                let cell = tableView.dequeueReusableCell(withIdentifier: "issueCell", for: IndexPath(row: row, section: 0))
+                cell.textLabel?.text = item.title
+                
+                return cell
+            }
+            .addDisposableTo(disposeBag)
+        
+        // Here we tell table view that if user clicks on a cell,
+        // and the keyboard is still visible, hide it
+        tableView
+            .rx.itemSelected
+            .subscribe(onNext: { indexPath in
+                if self.searchBar.isFirstResponder == true {
+                    self.view.endEditing(true)
+                }
+            })
+            .addDisposableTo(disposeBag)
     }
+    
 }
-
